c6de2a7e8c4f666107f69a83ef6e7b70a9c2f8fc
Eduardo Soravilla <dodonpachi@gmail.com>
2012-09-25 15:21:36 +0200

    Primer commit de la BD

--- DjangoLab/settings.py
+++ DjangoLab/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/esoravilla/Google Drive/Django/DjangoLab/djangolab.sqlite',                             # Or path to database file if using sqlite3.
+        'NAME': '/Users/esoravilla/Google Drive/Django/DjangoLab/djangolab.sqlite', # Or path to database file if using sqlite3.
         'USER': '',                             # Not used with sqlite3.
         'PASSWORD': '',                         # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
@@ -115,7 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'Potato',
+    'polls',
+    'polls'
 )
 
 # A sample logging configuration. The only tangible logging
