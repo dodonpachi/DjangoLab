e3d5f4e6d83be6f8ef0af492a10c77cf122c9922
Eduardo Soravilla <dodonpachi@gmail.com>
2012-09-25 16:50:26 +0200

    Cambios de estructura

--- DjangoLab/urls.py
+++ DjangoLab/urls.py
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'polls.views.home', name='home'),
+    # url(r'^$', 'polls.views.home', name='home'),
     # url(r'^DjangoLab/', include('DjangoLab.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
